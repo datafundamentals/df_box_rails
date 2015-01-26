ef428d4bd617d727fc5696c67881a53e70de02e7
jcarapet <jeffcarapetyan@gmail.com>
2015-01-26 14:36:15 -0600

    initial working copy of the rails app with nginx reverse proxy setup

--- Berksfile
+++ Berksfile
@@ -2,7 +2,7 @@ source "https://supermarket.getchef.com"
 
 metadata
 
-cookbook "df_nginx"
-cookbook "df_rails" 
-cookbook "df_php"
+cookbook "df_nginx", git: "https://github.com/datafundamentals/df_nginx"
+cookbook "df_rails", git: "https://github.com/datafundamentals/df_rails"
+cookbook "df_php", git: "https://github.com/datafundamentals/df_php"
 cookbook "apt"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,37 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "apt"
+
+execute "apt-get update" do 
+	ignore_failure true 
+end
 include_recipe "df_rails::default"
 include_recipe "df_nginx::default"
 include_recipe "df_php::default"
+
+execute "rails new public" do 
+	cwd "/home/app/mysite/current"
+	not_if do ::File.exists?('/home/app/mysite/current/public/config/unicorn.rb') end
+	action :run
+end
+
+template "/home/app/mysite/current/public/config/unicorn.rb" do 
+	source "unicornrb.erb"
+	action :create 
+end
+
+directory "/home/app/mysite/current/public/pids" do
+	recursive true
+	mode "0777" 
+	action :create
+end
+
+file "/home/app/mysite/current/public/log/unicorn.log" do 
+	mode "0777"
+	action :create
+end
+
+execute "unicorn_rails -c config/unicorn.rb -D" do 
+	cwd "/home/app/mysite/current/public" 
+	action :run 
+end
